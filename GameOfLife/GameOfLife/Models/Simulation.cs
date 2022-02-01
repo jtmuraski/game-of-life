7b5afa67fad2c489eb8e340e56a119f8125a4897
John Muraski <62039431+jtmuraski@users.noreply.github.com>
2022-01-31 23:00:06 -0600

    Initial commit

---
+++ GameOfLife/GameOfLife/Models/Simulation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife.Models
+{
+    public class Simulation
+    {
+        private int[,] Board { get; set; }
+        private int _height { get; set; }
+        private int _width { get; set; }
+        private int _numGenerations { get; set; }
+
+        public Simulation(int height, int width, int numGenerations)
+        {
+            _height = height;
+            _width = width;
+            _numGenerations = numGenerations;
+            Board = new int[height, width];
+            var random = new Random();
+            for (int i = 0; i < height; i++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Board[i, x] = random.Next(0, 1);
+                }
+            }
+        }
+
+        // Return a String that contains a printout of the board
+        public string PrintBoard()
+        {
+            // Set up strings for the borders and empty spaces within the baord
+            StringBuilder board = new StringBuilder();
+            string topper = "";
+            for(int i = 0; i < _width; i++)
+            {
+                topper += "------";
+            }
+
+            string topInnerRow = "";
+            for (int i = 0; i < (_width - 1); i++)
+            {
+                topInnerRow += "|     ";
+            }
+            topInnerRow += "|    |";
+            board.AppendLine(topper);
+
+            // Generate each row of the board
+            for (int y = 0; y < _height; y++)
+            {
+                string row = "";
+                for(int x = 0; x < _width; x++)
+                {
+                    if (x < (_width - 1))
+                        row += "|  " + Board[y, x] + "  ";
+                    else
+                        row += "|  " + Board[y, x] + " |";
+                }
+                board.AppendLine(topInnerRow);
+                board.AppendLine(row);
+                board.AppendLine(topInnerRow);
+                board.AppendLine(topper);
+            }
+
+            return board.ToString();
+        }
+    }
+}
---
+++ GameOfLife/GameOfLife/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using Console = Colorful.Console;
+using Colorful;
+using GameOfLife.Models;
+
+// Author: John Muraski
+// Description: This is my take of the Conway Game Of Life as a coding challenge
+// Game of Life Rules:
+// Cells on the board are represented by 1's and 0's. One represents a live cell, and a 0 represents a dead cell
+// Each generation applies the following 4 rules:
+// 1) Any live cell with fewer than two live neighbors dies as if caused by under-population.
+// 2) Any live cell with two or three live neighbors lives on to the next generation.
+// 3) Any live cell with more than three live neighbors dies, as if by over-population.
+// 4) Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
+namespace GameOfLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool runApp = true;
+            do
+            {
+                // Display the menu for the application
+                Console.WriteAscii("Game Of Life", Color.DarkGreen);
+                Console.WriteLine();
+                Console.WriteLine("Welcome to the Game Of Life simulation, my take on Conway's Game Of Life simulation");
+                Console.WriteLine("Choose from one of the options below to continue");
+                Console.WriteLine("1. View Descripiton and Simulation Rules", Color.Blue);
+                Console.WriteLine("2. Setup and begin a new simulation", Color.Blue);
+                Console.WriteLine("3. Load a previous simulation set up", Color.Blue);
+                Console.WriteLine("4. Exit", Color.Red);
+                string choice = Console.ReadLine();
+                switch(choice)
+                {
+                    case ("2"):
+                        runApp = SetUpSim();
+                        break;
+                    case ("4"):
+                        runApp = false;
+                        break;
+                        
+                }
+            } while (runApp);
+        }
+
+        private static bool SetUpSim()
+        {
+            int boardHeight = 0;
+            int boardWidth = 0;
+            int numGenerations = 0;
+
+            bool validHeight = false;
+            while(!validHeight)
+            {
+                Console.WriteLine("How tall shall the board be?");
+                string height = Console.ReadLine();
+                validHeight = Int32.TryParse(height, out boardHeight);
+                if (!validHeight)
+                    Console.WriteLine("You did not enter a valid integer value. Enter a integer value and try again", Color.DarkRed);
+            }
+
+            bool validWidth = false;
+            while (!validWidth)
+            {
+                Console.WriteLine("How wide shall the board be?");
+                string width = Console.ReadLine();
+                validWidth = Int32.TryParse(width, out boardWidth);
+                if (!validWidth)
+                    Console.WriteLine("You did not enter a valid integer value. Enter a integer value and try again", Color.DarkRed);
+            }
+
+            bool validGeneration = false;
+            while (!validGeneration)
+            {
+                Console.WriteLine("How many generations do you want to simulate?");
+                string width = Console.ReadLine();
+                validGeneration = Int32.TryParse(width, out numGenerations);
+                if (!validGeneration)
+                    Console.WriteLine("You did not enter a valid integer value. Enter a integer value and try again", Color.DarkRed);
+            }
+
+            Simulation board = new Simulation(boardHeight, boardWidth, numGenerations);
+            Console.WriteLine(board.PrintBoard());
+            Console.ReadLine();
+            return true;
+        }
+    }
+}
